010130b3234305973c98f072b782c49671dda03d
uruly <rk.uru007@gmail.com>
2020-07-20 15:17:56 +0900

    :rocket: Add UITextView

--- UITextViewFontIssue/ViewController.swift
+++ UITextViewFontIssue/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet private weak var textView: UITextView! {
+        didSet {
+            textView.text = "UITextView コードからtext指定"
+        }
+    }
+    @IBOutlet private weak var textView2: UITextView! {
+        didSet {
+            textView2.text = "UITextView コードからtext指定（初期テキストあり）"
+        }
+    }
+    @IBOutlet weak var textView3: UITextView! {
+        didSet {
+            textView3.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+            textView3.text = "UITextView コードからtext, font指定（初期テキスト有り）"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
